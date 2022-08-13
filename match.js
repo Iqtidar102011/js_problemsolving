d50a3b38280f0d1a6f9c0621ac33109e12b99b2e
Iqtidar102011 <iqidarwasif@gmail.comm>
2022-08-13 12:42:42 +0200

    matched item prob solved

--- match.js
+++ match.js
@@ -11,6 +11,16 @@
 // }
 
 // find the mathced products from this array
+products = [
+    {
+        id: 1, name: "name1", price: 19000,
+    },
+    { id: 2, name: "name2", price: 19000, },
+    { id: 3, name: "name3", price: 24234324, }
+
+
+
+]
 
 function matched(products, search) {
     // to store the matched items in a new array
@@ -19,7 +29,7 @@ function matched(products, search) {
     // use include of to check if the value is there or not
     // use .push()to push the matched items in the new array
     for (const product of products) {
-        if (product.price.includes(search)) {
+        if (product.price.toString().includes(search)) {
             match.push(product);
         }
     }
